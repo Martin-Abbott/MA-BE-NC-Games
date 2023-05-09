2b1710c64ceea6d4907c4a53d8d4870f2c53cf96
Martin Abbott <abbott01@googlemail.com>
2023-05-09 16:49:06 +0100

    Completed task 3.5 on branch task-3.5 - Submitting for review

--- controllers.js
+++ controllers.js
@@ -1,9 +1,21 @@
 const { selectCategories } = require("./models");
+const fs = require("fs/promises");
 
 exports.getCategories = (req, res, next) => {
 	selectCategories()
 		.then((categories) => {
 			res.status(200).send({ categories });
+		})
+		.catch((err) => {
+			next(err);
+		});
+};
+
+exports.getAPI = (req, res, next) => {
+	fs.readFile(`${__dirname}/endpoints.json`)
+		.then((endpoints) => {
+			const parsedEndpoints = JSON.parse(endpoints);
+			res.status(200).send({ endpoints: parsedEndpoints });
 		})
 		.catch((err) => {
 			next(err);
